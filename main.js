9b131b5790a1999fbde418ae07cfccf8c1ad419b
Nguyen Phu Minh <53084840+nguyenphuminh@users.noreply.github.com>
2020-08-04 13:55:37 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	var changeFonts = document.querySelector("body");
+	changeFonts.style.fontFamily="helvetica";
+}
+
 var blockAd;
 delLogo = setInterval(function(){
 	blockAd = document.querySelector('[alt="www.000webhost.com"]');
